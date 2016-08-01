65d18b42ea5fcaacde58b18f6b6b2d20c414d292
Aixa Rodriguez <aixa.rodriguez@gmail.com>
2016-07-31 18:58:45 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix - Works like a class that define the usign of solve function to obtains the cache result 
+## of inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   s<-NULL
@@ -17,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function obteins the cache value of a matrix inverse previously calculated
 
 cacheSolve <- function(x,...) {
         ## Return a matrix that is the inverse of 'x'
@@ -31,3 +32,27 @@ cacheSolve <- function(x,...) {
   x$setSolve(s)
   s
 }
+
+#Using Example
+#> my_mat<-matrix(rnorm(1:16),nrow=4,ncol=4)
+#> mmat<-makeCacheMatrix()
+#> mmat$set(my_mat)
+#> mmat$get()
+#           [,1]       [,2]       [,3]       [,4]
+#[1,] -1.2294798 -0.2102174  0.4401325  0.4806962
+#[2,] -0.9725740 -0.3798900 -0.5601481  0.2658546
+#[3,]  0.2315410 -0.5423295 -0.2496990 -1.0628247
+#[4,] -0.2414938 -1.1294815 -2.1869005 -0.1940190
+#> cacheSolve(mmat)
+#          [,1]      [,2]         [,3]       [,4]
+#[1,]  0.837416 -2.377216 -0.365425093  0.8191562
+#[2,] -3.227276  4.530238  0.004032427 -1.8103431
+#[3,]  1.442109 -1.865091  0.131736008  0.2956476
+#[4,]  1.490415 -2.391357 -1.053505993  1.0327648
+#> cacheSolve(mmat)
+#Getting Cached Data
+#          [,1]      [,2]         [,3]       [,4]
+#[1,]  0.837416 -2.377216 -0.365425093  0.8191562
+#[2,] -3.227276  4.530238  0.004032427 -1.8103431
+#[3,]  1.442109 -1.865091  0.131736008  0.2956476
+#[4,]  1.490415 -2.391357 -1.053505993  1.0327648
